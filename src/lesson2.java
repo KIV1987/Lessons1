f8a234a59e84ffd3666fdeb99ba0b7befee90fa8
KIV1987 <ivankudinr@mail.ru>
2022-11-16 15:19:31 +0300

    finish 2

--- src/lesson2.java
+++ src/lesson2.java
@@ -22,14 +22,14 @@ public class lesson2 {
             } else //ФИО
             {
                 String[] FIO = string.split(" ");
-                outPutString += FIO[0].substring(0,1)+FIO[0].substring(1,FIO[0].length()-1).replaceAll(".", "*") + FIO[0].substring(FIO[0].length()-1)  + " "+
-                        FIO[1] +" "+ FIO[2].substring(0,1)+".;";
+                outPutString += FIO[0].charAt(0)+FIO[0].substring(1,FIO[0].length()-1).replaceAll(".", "*") + FIO[0].charAt(FIO[0].length()-1)  + " "+
+                        FIO[1] +" "+ FIO[2].charAt(0)+".;";
             }
         }
         return outPutString.substring(0,outPutString.length()-1) + inputText.substring(inputText.indexOf("</data>"));
     }
     public static void main(String[] args) {
-    String inputText = "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Иванов Иван Иванович</data></client>",outPutString ="";
+    String inputText = "<client>(Какие то данные)<data>79991113344;test@yandex.ru;Ivanov Ivan Ivanovich</data></client>";
         System.out.println(Mask(inputText));
     }
 }
