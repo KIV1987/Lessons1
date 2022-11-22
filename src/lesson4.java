56f616b4064ad0330dd9364e665fd941d67842c4
KIV1987 <ivankudinr@mail.ru>
2022-11-22 09:48:05 +0300

    finish 2

---
+++ src/lesson4.java
@@ -0,0 +1,30 @@
+
+import java.util.HashSet;
+        import java.util.Map;
+        import java.util.Set;
+        import java.util.regex.Pattern;
+
+public class lesson4 {
+    public static Set FinderCar(String regex, Map<Integer, Map<String, String[]>> data) {
+        HashSet<String> setCars = new HashSet<>();
+        for (Map.Entry<Integer, Map<String, String[]>> region:data.entrySet()        ) {
+            for (Map.Entry<String, String[]> arrayNumbers : region.getValue().entrySet()) {
+                for (String number : arrayNumbers.getValue()) {
+                    if (Pattern.matches(regex,number)){
+                        setCars.add(number);
+                    }
+                }
+            }
+        }
+        return setCars;
+    }
+
+    public static void main(String[] args) {
+        final String FIRST = "М";
+        final String SECOND = "АВ";
+        String regex = "("+FIRST+")\\w+("+SECOND+")\\w+";
+        Map<Integer, Map<String, String[]>> data = GeneratorExpertHomework.getData();
+        FinderCar(regex,data).stream().forEach(System.out::println);
+        System.out.println(FinderCar(regex,data).size());
+    }
+}
